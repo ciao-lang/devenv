90e9cc9cae6dde3f55cff910a45706c95275db78
Jose F. Morales <jfmcjf@gmail.com>
2016-12-31 11:45:48 +0100

    Public snapshot for cb3842fba5bd886718ecec7862bc644eab211fe4
    Src-commit: cb3842fba5bd886718ecec7862bc644eab211fe4

--- Manifest.pl
+++ Manifest.pl
@@ -1,14 +1,14 @@
 :- bundle(devenv).
 % The default development environment
-version('1.15.0').
+version('1.16.0-alpha.1').
 alias_paths([]).
 depends([
   builder-[version>='1.3.0-alpha.1'],
-  core-[version>='1.15.0'],
-  contrib-[version>='1.15.0'],
+  core-[version>='1.16.0-alpha.1'],
+  contrib-[version>='1.16.0-alpha.1'],
   lpdoc-[version>='3.0.0'],
   % ciaopp-[version='1.2.0'], (Optional)
-  ciao_emacs-[version>='1.15.0'],
+  ciao_emacs-[version>='1.16.0-alpha.1'],
   % Include general manuals
-  alldocs-[version>='1.15.0']
+  alldocs-[version>='1.16.0-alpha.1']
 ]).
