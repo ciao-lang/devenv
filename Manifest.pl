0ed0f41b98cb937427fb717806f02b385f5e12fc
Jose F. Morales <jfmcjf@gmail.com>
2017-01-23 19:17:41 +0100

    Public snapshot for a0e4705798ce994fd1743b6bba2d20e71eebe2a6
    Src-commit: a0e4705798ce994fd1743b6bba2d20e71eebe2a6

--- Manifest.pl
+++ Manifest.pl
@@ -5,12 +5,12 @@ alias_paths([]).
 depends([
   builder-[version>='1.3.0-alpha.1'],
   core-[version>='1.16.0-alpha.1'],
-  contrib-[version>='1.16.0-alpha.1'],
   lpdoc-[version>='3.0.0'],
   % ciaopp-[version='1.2.0'], (Optional)
   ciao_emacs-[version>='1.16.0-alpha.1'],
   ciaodbg-[version>='1.16.0-alpha.1'],
   ciaofmt-[version>='1.0.0'],
+  % TODO: add more bundles?
   % Include general manuals
   alldocs-[version>='1.16.0-alpha.1']
 ]).
