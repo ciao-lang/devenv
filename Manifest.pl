10674c56b37bb32cacd60c3495f64ec729a2a541
Jose F. Morales <jfmcjf@gmail.com>
2017-01-25 13:13:08 +0100

    Public snapshot for 92d5c6d7fbdcf0a8f0cf145be33372970f380867
    Src-commit: 92d5c6d7fbdcf0a8f0cf145be33372970f380867

--- Manifest.pl
+++ Manifest.pl
@@ -1,16 +1,16 @@
 :- bundle(devenv).
 % The default development environment
-version('1.16.0-alpha.1').
+version('1.16.0-alpha.2').
 alias_paths([]).
 depends([
-  builder-[version>='1.3.0-alpha.1'],
-  core-[version>='1.16.0-alpha.1'],
+  builder-[version>='1.4.0'],
+  core-[version>='1.16.0-alpha.2'],
   lpdoc-[version>='3.0.0'],
   % ciaopp-[version='1.2.0'], (Optional)
-  ciao_emacs-[version>='1.16.0-alpha.1'],
-  ciaodbg-[version>='1.16.0-alpha.1'],
+  ciao_emacs-[version>='1.16.0-alpha.2'],
+  ciaodbg-[version>='1.16.0-alpha.2'],
   ciaofmt-[version>='1.0.0'],
   % TODO: add more bundles?
   % Include general manuals
-  alldocs-[version>='1.16.0-alpha.1']
+  alldocs-[version>='1.16.0-alpha.2']
 ]).
