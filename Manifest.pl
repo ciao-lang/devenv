694a32d3beb60cf5e9637b1e40cc8a4800204d0f
Jose F. Morales <jfmcjf@gmail.com>
2018-12-06 12:24:25 +0100

    Updated version numbers
    Src-commit: 6947d8915b61774272279812e42a653bc48117ff

--- Manifest.pl
+++ Manifest.pl
@@ -1,16 +1,16 @@
 :- bundle(devenv).
 % The default development environment
-version('1.16.0-alpha.3').
+version('1.18.0').
 alias_paths([]).
 depends([
-  builder-[version>='1.5.0'],
-  core-[version>='1.16.0-alpha.3'],
-  lpdoc-[version>='3.0.0'],
+  builder-[version>='1.18'],
+  core-[version>='1.18'],
+  lpdoc-[version>='3.2.0'],
   % ciaopp-[version='1.2.0'], (Optional)
-  ciao_emacs-[version>='1.16.0-alpha.3'],
-  ciaodbg-[version>='1.16.0-alpha.3'],
+  ciao_emacs-[version>='1.18'],
+  ciaodbg-[version>='1.18'],
   ciaofmt-[version>='1.0.0'],
   % TODO: add more bundles?
   % Include general manuals
-  alldocs-[version>='1.16.0-alpha.3']
+  alldocs-[version>='1.18']
 ]).
