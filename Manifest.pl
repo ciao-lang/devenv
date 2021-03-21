a79faef5260244e6ef762ac761b5573a9ed8ebec
Jose F. Morales <jfmcjf@gmail.com>
2021-03-21 01:58:18 +0100

    Bump version to 1.20
    Src-commit: 68ef9e8b0483d0eeecd9c15347228a0e47d6bdba

--- Manifest.pl
+++ Manifest.pl
@@ -1,16 +1,16 @@
 :- bundle(devenv).
 % The default development environment
-version('1.19.0').
+version('1.20.0').
 alias_paths([]).
 depends([
-  builder-[version>='1.19'],
-  core-[version>='1.19'],
+  builder-[version>='1.20'],
+  core-[version>='1.20'],
   lpdoc-[version>='3.2.0'],
   % ciaopp-[version='1.2.0'], (Optional)
-  ciao_emacs-[version>='1.19'],
-  ciaodbg-[version>='1.19'],
+  ciao_emacs-[version>='1.20'],
+  ciaodbg-[version>='1.20'],
   ciaofmt-[version>='1.0.0'],
   % TODO: add more bundles?
   % Include general manuals
-  alldocs-[version>='1.19']
+  alldocs-[version>='1.20']
 ]).
