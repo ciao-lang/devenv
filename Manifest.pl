187411d47df7bab62f57c57c1199ad5b4750767a
Jose F. Morales <jfmcjf@gmail.com>
2021-04-26 19:51:33 +0200

    (devenv) Include ciaopp by default
    Src-commit: d1b36de36d741a4a05aad6bddeae74c263d16162

--- Manifest.pl
+++ Manifest.pl
@@ -6,7 +6,7 @@ depends([
   builder-[version>='1.20'],
   core-[version>='1.20'],
   lpdoc-[version>='3.2.0'],
-  % ciaopp-[version='1.2.0'], (Optional)
+  ciaopp-[version='1.4.0'],
   ciao_emacs-[version>='1.20'],
   ciaodbg-[version>='1.20'],
   ciaofmt-[version>='1.0.0'],
