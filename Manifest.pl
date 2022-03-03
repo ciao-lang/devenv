4e3c6abc587893e0a8eea40e4d2f8c492dac68a1
Jose F. Morales <jfmcjf@gmail.com>
2022-03-03 09:23:43 +0100

    Updated versions
    Src-commit: 471a8d28f155296b6951c2c611c0f2aad1a3c8c7

--- Manifest.pl
+++ Manifest.pl
@@ -1,16 +1,16 @@
 :- bundle(devenv).
 % The default development environment
-version('1.21.0').
+version('1.21.0'). % (same as 'core')
 alias_paths([]).
 depends([
-  builder-[version>='1.20'],
-  core-[version>='1.20'],
+  builder-[version>='1.21'], % (same as 'core')
+  core-[version>='1.21'],
   lpdoc-[version>='3.2.0'],
   ciaopp-[version='1.4.0'],
-  ciao_emacs-[version>='1.20'],
-  ciaodbg-[version>='1.20'],
+  ciao_emacs-[version>='1.21'], % (same as 'core')
+  ciaodbg-[version>='1.21'], % (same as 'core')
   ciaofmt-[version>='1.0.0'],
   % TODO: add more bundles?
   % Include general manuals
-  alldocs-[version>='1.20']
+  alldocs-[version>='1.21'] % (same as 'core')
 ]).
