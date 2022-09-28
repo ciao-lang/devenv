91d52fc79693dff51894b6a75aee019d1e00a206
Jose F. Morales <jfmcjf@gmail.com>
2022-09-28 17:42:18 +0200

    Updating version 1.22.0
    Src-commit: 3b5f0895bab4700718bc79deed54fffdc1dcd374

--- Manifest.pl
+++ Manifest.pl
@@ -1,16 +1,16 @@
 :- bundle(devenv).
 % The default development environment
-version('1.21.0'). % (same as 'core')
+version('1.22.0'). % (same as 'core')
 alias_paths([]).
 depends([
-  builder-[version>='1.21'], % (same as 'core')
-  core-[version>='1.21'],
+  builder-[version>='1.22'], % (same as 'core')
+  core-[version>='1.22'],
   lpdoc-[version>='3.5.0'],
   ciaopp-[version='1.5.0'],
-  ciao_emacs-[version>='1.21'], % (same as 'core')
-  ciaodbg-[version>='1.21'], % (same as 'core')
+  ciao_emacs-[version>='1.22'], % (same as 'core')
+  ciaodbg-[version>='1.22'], % (same as 'core')
   ciaofmt-[version>='1.0.0'],
   % TODO: add more bundles?
   % Include general manuals
-  alldocs-[version>='1.21'] % (same as 'core')
+  alldocs-[version>='1.22'] % (same as 'core')
 ]).
