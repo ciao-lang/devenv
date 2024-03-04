01f8531bc442a5a7fde084ac4ddb1c6b64312fd3
Jose F. Morales <jfmcjf@gmail.com>
2024-03-04 18:28:27 +0100

    Bump Ciao version to 1.23
    Src-commit: bf3a44dc1215a94a0592bb3d196580c48e7f8955

--- Manifest.pl
+++ Manifest.pl
@@ -1,16 +1,16 @@
 :- bundle(devenv).
 % The default development environment
-version('1.22.0'). % (same as 'core')
+version('1.23.0'). % (same as 'core')
 alias_paths([]).
 depends([
-  builder-[version>='1.22'], % (same as 'core')
-  core-[version>='1.22'],
+  builder-[version>='1.23'], % (same as 'core')
+  core-[version>='1.23'],
   lpdoc-[version>='3.6.0'],
   ciaopp-[version='1.6.0'],
-  ciao_emacs-[version>='1.22'], % (same as 'core')
-  ciaodbg-[version>='1.22'], % (same as 'core')
+  ciao_emacs-[version>='1.23'], % (same as 'core')
+  ciaodbg-[version>='1.23'], % (same as 'core')
   ciaofmt-[version>='1.0.0'],
   % TODO: add more bundles?
   % Include general manuals
-  alldocs-[version>='1.22'] % (same as 'core')
+  alldocs-[version>='1.23'] % (same as 'core')
 ]).
