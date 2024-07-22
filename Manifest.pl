3c9b9782c4161c2d76182ddb2596f3055c2d1470
Jose F. Morales <jfmcjf@gmail.com>
2024-07-22 19:34:52 +0200

    (devenv) update deps
    Src-commit: bcfdd3b18a57c51394694db5bde0a29e3b04c521

--- Manifest.pl
+++ Manifest.pl
@@ -6,7 +6,7 @@ depends([
   builder-[version>='1.23'], % (same as 'core')
   core-[version>='1.23'],
   lpdoc-[version>='3.6.0'],
-  ciaopp-[version='1.6.0'],
+  ciaopp-[version>='1.7.0'],
   ciao_emacs-[version>='1.23'], % (same as 'core')
   ciaofmt-[version>='1.0.0'],
   % TODO: add more bundles?
