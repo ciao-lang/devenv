43fcf3f91657a5640af256e2a9da5ba8e3beeafa
Jose F. Morales <jfmcjf@gmail.com>
2025-06-17 17:47:28 +0200

    Jump version to 1.25
    Src-commit: 1174c3364d0d3ec61f21ac80fdf5116172d7acb8

--- Manifest.pl
+++ Manifest.pl
@@ -1,15 +1,15 @@
 :- bundle(devenv).
 % The default development environment
-version('1.24.0'). % (same as 'core')
+version('1.25.0'). % (same as 'core')
 alias_paths([]).
 depends([
-  builder-[version>='1.24'], % (same as 'core')
-  core-[version>='1.24'],
+  builder-[version>='1.25'], % (same as 'core')
+  core-[version>='1.25'],
   lpdoc-[version>='3.6.0'],
   ciaopp-[version>='1.7.0'],
-  ciao_emacs-[version>='1.24'], % (same as 'core')
+  ciao_emacs-[version>='1.25'], % (same as 'core')
   ciaofmt-[version>='1.0.0'],
   % TODO: add more bundles?
   % Include general manuals
-  alldocs-[version>='1.24'] % (same as 'core')
+  alldocs-[version>='1.25'] % (same as 'core')
 ]).
